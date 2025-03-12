a1043ef75101fa1cb7463556fb403cb74957d78d
Gyorgy Attila <gyorgyattxla@gmail.com>
2025-03-12 15:15:52 +0100

    Initial commit.

---
+++ src/main/java/com/antilope/antilopeManager/AntilopeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.antilope.antilopeManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntilopeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntilopeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antilope/antilopeManager/AntilopeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antilope.antilopeManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntilopeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
