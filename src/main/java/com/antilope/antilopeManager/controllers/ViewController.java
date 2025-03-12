59d8be4b0f0c85ef260f579d28f17b43b3896ff0
Gyorgy Attila <gyorgyattxla@gmail.com>
2025-03-12 21:32:53 +0100

    Readded Thymeleaf & created index.html prototype.

--- src/main/java/com/antilope/antilopeManager/controllers/ViewController.java
+++ src/main/java/com/antilope/antilopeManager/controllers/ViewController.java
@@ -1,13 +1,15 @@
 package com.antilope.antilopeManager.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("message", "homeIcon");
         return "index";
     }
 }
