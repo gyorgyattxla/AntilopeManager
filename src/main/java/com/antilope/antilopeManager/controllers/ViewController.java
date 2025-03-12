df67b395608ded4afaa37d57019756ac72aecb6f
Gyorgy Attila <gyorgyattxla@gmail.com>
2025-03-12 17:50:08 +0100

    ViewController prototype.

---
+++ src/main/java/com/antilope/antilopeManager/controllers/ViewController.java
@@ -0,0 +1,13 @@
+package com.antilope.antilopeManager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
